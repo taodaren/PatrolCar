649c2c298aa1ec11fc35635541832a47f13dd5cd
taodaren <397771543@qq.com>
2018-08-24 09:47:25 +0800

    first commit

---
+++ app/src/main/java/patrolcar/bobi/cn/patrolcar/MainActivity.java
@@ -0,0 +1,13 @@
+package patrolcar.bobi.cn.patrolcar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
