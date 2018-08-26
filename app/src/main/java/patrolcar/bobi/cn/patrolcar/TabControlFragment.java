c6d3e36d752d723e1f06b44503d8aa2647483c29
taodaren <taodaren1125@gmail.com>
2018-08-27 07:50:10 +0800

    ctrl view layout and click

--- app/src/main/java/patrolcar/bobi/cn/patrolcar/TabControlFragment.java
+++ app/src/main/java/patrolcar/bobi/cn/patrolcar/TabControlFragment.java
@@ -1,12 +1,28 @@
 package patrolcar.bobi.cn.patrolcar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 遥控模块
  */
 
 public class TabControlFragment extends BaseFragment {
+
+    @BindView(R.id.btn_ctrl_stop)        Button btnStop;
+    @BindView(R.id.rl_ctrl_up)           RelativeLayout btnUp;
+    @BindView(R.id.rl_ctrl_down)         RelativeLayout btnDown;
+    @BindView(R.id.rl_ctrl_left)         RelativeLayout btnLeft;
+    @BindView(R.id.rl_ctrl_right)        RelativeLayout btnRight;
+    @BindView(R.id.open_device)          TextView btnDevOpen;
+    @BindView(R.id.close_device)         TextView btnDevClose;
+    @BindView(R.id.brake_start)          TextView btnBrekeStart;
+    @BindView(R.id.brake_release)        TextView btnBrekeRelease;
 
     public static TabControlFragment newInstance() {
         return new TabControlFragment();
@@ -32,8 +48,38 @@ public class TabControlFragment extends BaseFragment {
 
     }
 
-    @Override
-    public void initListener() {
 
+    @OnClick({R.id.btn_ctrl_stop, R.id.rl_ctrl_up, R.id.rl_ctrl_down, R.id.rl_ctrl_left, R.id.rl_ctrl_right, R.id.open_device, R.id.close_device, R.id.brake_start, R.id.brake_release})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_ctrl_stop:
+                ToastTools.showShort(getContext(), "stop");
+                break;
+            case R.id.rl_ctrl_up:
+                ToastTools.showShort(getContext(), "up");
+                break;
+            case R.id.rl_ctrl_down:
+                ToastTools.showShort(getContext(), "down");
+                break;
+            case R.id.rl_ctrl_left:
+                ToastTools.showShort(getContext(), "left");
+                break;
+            case R.id.rl_ctrl_right:
+                ToastTools.showShort(getContext(), "right");
+                break;
+            case R.id.open_device:
+                ToastTools.showShort(getContext(), "open");
+                break;
+            case R.id.close_device:
+                ToastTools.showShort(getContext(), "close");
+                break;
+            case R.id.brake_start:
+                ToastTools.showShort(getContext(), "start");
+                break;
+            case R.id.brake_release:
+                ToastTools.showShort(getContext(), "release");
+                break;
+        }
     }
+
 }
